084e66766aea366b33ee11288d62c3c66f4e9c81
griguk77 <griguk77@gmail.com>
2025-09-27 02:55:04 +0300

    first commit

---
+++ precache-manifest.cdde71a95161c59fe1cb4ae232e5d600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6438610a98cdc6a3813a5b19752837f8",
+    "url": "/yandex-translation/index.html"
+  },
+  {
+    "revision": "6a5b77dd7c779a073ba6",
+    "url": "/yandex-translation/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/yandex-translation/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "6a5b77dd7c779a073ba6",
+    "url": "/yandex-translation/static/js/main.165f9cc3.chunk.js"
+  },
+  {
+    "revision": "f0ee3328bd549872c030",
+    "url": "/yandex-translation/static/js/runtime~main.86dff5c6.js"
+  }
+]);
